0bd3fc6860137eedf751c052ce5266e365cfaf77
Nijumdip <nijumdip019911@gmail.com>
2022-05-23 21:13:23 +0600

    added mongodb

--- index.js
+++ index.js
@@ -2,10 +2,73 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zm5jm.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("green-bikester").collection("products");
+
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+    
+    
+  } finally {
+    // client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Green Bikester!')
